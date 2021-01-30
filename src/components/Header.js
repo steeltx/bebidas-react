0bf9247dd2c4a1ef00a3d65ba34dfc5f4b947807
steeltx <realsteel.gnu@gmail.com>
2021-01-30 16:19:38 -0600

    Funcionalidad terminada

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="bg-alert">
+            <h1>Busca recetas de Bebidas <small>(steeltx)</small></h1>
+        </header>
+    );
+}
+ 
+export default Header;
---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { createContext, useState, useEffect } from 'react';
+
+// crear el context
+export const CategoriasContext = createContext();
+
+// provider donde se encuentran las funciones y state
+const CategoriasProvider = (props) => {
+
+    // crear el state del context
+    const [categorias, guardarCategorias] = useState([]);
+
+    // ejecutar el llamado a la api
+    useEffect(() => {
+        const obtenerCategorias = async () => {
+            const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+            const categorias = await axios.get(url);
+            guardarCategorias(categorias.data.drinks);
+        }
+        obtenerCategorias();
+    },[]);
+
+    return (
+        <CategoriasContext.Provider
+            value={{
+                categorias
+            }}
+        >
+            {props.children}
+        </CategoriasContext.Provider>
+    )
+}
+
+export default CategoriasProvider;
